52e844e70668e1cabe8f619cf61b62d0f215693b
Erik Holum <eholum@gmail.com>
2025-06-17 08:58:13 -0400

    No hardcoded example paths, without ament

--- roboplan_examples/examples/test_scene.cpp
+++ roboplan_examples/examples/test_scene.cpp
@@ -1,19 +1,19 @@
 #include <filesystem>
+#include <roboplan/core/scene.hpp>
 #include <vector>
 
-#include <roboplan/core/scene.hpp>
-
 int main(int /*argc*/, char* /*argv*/[]) {
+
+  const std::string install_prefix = TEST_SCENE_INSTALL_PREFIX;
+
   const std::filesystem::path urdf_path{
-      "/home/sebastian/workspace/roboplan_ws/src/roboplan/"
-      "roboplan_examples/ur_robot_model/ur5_gripper.urdf"};
-
+      install_prefix +
+      "/share/roboplan_examples/ur_robot_model/ur5_gripper.urdf"};
   const std::filesystem::path srdf_path{
-      "/home/sebastian/workspace/roboplan_ws/src/roboplan/"
-      "roboplan_examples/ur_robot_model/ur5_gripper.srdf"};
-
+      install_prefix +
+      "/share/roboplan_examples/ur_robot_model/ur5_gripper.srdf"};
   const std::vector<std::filesystem::path> package_paths{
-      "/home/sebastian/workspace/roboplan_ws/src/roboplan/"};
+      install_prefix + "/share/roboplan_examples/"};
 
   auto scene =
       roboplan::Scene("test_scene", urdf_path, srdf_path, package_paths);
