737aae790cf1a96f556ea2a8dc488876232c7f25
Sebastian Castro <4603398+sea-bass@users.noreply.github.com>
2025-06-20 14:28:35 -0400

    ok erik, weasel yourself into this contribution
    Co-authored-by: Erik Holum <7966037+eholum@users.noreply.github.com>

--- roboplan_example_models/src/resources.cpp
+++ roboplan_example_models/src/resources.cpp
@@ -4,7 +4,7 @@ namespace roboplan_example_models {
 
 std::filesystem::path get_install_prefix() {
   // This would be a lot easier if it were an ament package, instead we use
-  // dynamic linking to get the filesystem path of the example executable.
+  // dynamic linking to get the filesystem path of the example resources shared object file.
   Dl_info dl_info;
   dladdr((void*)get_install_prefix, &dl_info);
   std::filesystem::path lib_path = dl_info.dli_fname;
