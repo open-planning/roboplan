6e78de9f3f72d71c8d605884c094cc1ef3e145d5
Sebastian Castro <sebas.a.castro@gmail.com>
2025-06-23 18:45:21 -0400

    Fix bug

--- roboplan/src/core/scene.cpp
+++ roboplan/src/core/scene.cpp
@@ -107,15 +107,19 @@ bool Scene::hasCollisionsAlongPath(const Eigen::VectorXd& q_start, const Eigen::
   const auto distance = pinocchio::distance(model_, q_start, q_end);
 
   // Special case for short paths (also handles division by zero in the next case).
+  const bool collision_at_endpoints = hasCollisions(q_start) || hasCollisions(q_end);
   if (distance <= min_step_size) {
-    return hasCollisions(q_start) && hasCollisions(q_end);
+    return collision_at_endpoints;
   }
 
+  // In the general case, check the first and last points, then all the intermediate ones.
   const auto num_steps = static_cast<size_t>(std::ceil(distance / min_step_size)) + 1;
-  for (size_t idx = 0; idx <= num_steps; ++idx) {
+  if (collision_at_endpoints) {
+    return true;
+  }
+  for (size_t idx = 1; idx <= num_steps - 1; ++idx) {
     const auto fraction = static_cast<double>(idx) / static_cast<double>(num_steps);
-    const auto q_interp = pinocchio::interpolate(model_, q_start, q_end, fraction);
-    if (hasCollisions(q_interp)) {
+    if (hasCollisions(pinocchio::interpolate(model_, q_start, q_end, fraction))) {
       return true;
     }
   }
--- roboplan/test/test_scene.cpp
+++ roboplan/test/test_scene.cpp
@@ -71,7 +71,6 @@ TEST_F(RoboPlanSceneTest, CollisionCheckAlongPath) {
   q_end_free << 1.0, -1.57, 1.57, 0.0, 0.0, 0.0;
   EXPECT_FALSE(scene_->hasCollisionsAlongPath(q_start_free, q_end_free, 0.05));
 
-  // In collision
   Eigen::VectorXd q_end_coll(6);
   q_end_coll << 0.0, -1.57, 3.0, 0.0, 0.0, 0.0;
   EXPECT_TRUE(scene_->hasCollisionsAlongPath(q_start_free, q_end_coll, 0.05));
