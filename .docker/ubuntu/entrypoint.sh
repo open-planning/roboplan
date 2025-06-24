d3010a2c89613ebc71a8504c2c17bdcb71ae8f65
Erik Holum <eholum@gmail.com>
2025-06-24 13:53:27 -0400

    Ok conda broke so why no pip

--- .docker/ubuntu/entrypoint.sh
+++ .docker/ubuntu/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Set the library paths
-pushd "/roboplan_ws/install" > /dev/null || exit
-for PACKAGE in */lib;
-do
-    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/roboplan_ws/install/${PACKAGE}"
-done
-popd > /dev/null || exit
-echo "Set LD_LIBRARY_PATH to installed packages."
-
 # Execute the command passed into this entrypoint
 exec "$@"
