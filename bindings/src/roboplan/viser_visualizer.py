80c4a26441f951afa78d05eab79c58458a79d723
Sebastian Castro <sebas.a.castro@gmail.com>
2025-07-24 22:40:35 -0400

    Optionally accept None as path in visualization

--- bindings/src/roboplan/viser_visualizer.py
+++ bindings/src/roboplan/viser_visualizer.py
@@ -340,7 +340,7 @@ def visualizePath(
     viz: ViserVisualizer,
     scene: Scene,
     rrt: RRT,
-    path: JointPath,
+    path: JointPath | None,
     frame_name: str,
     max_step_size: float,
 ) -> None:
@@ -351,7 +351,7 @@ def visualizePath(
         viz: The viser visualizer instance.
         scene: The scene instance.
         rrt: The RRT planner instance.
-        path: The joint path to visualize.
+        path: The joint path to visualize. If None, does not visualize the path.
         frame_name: The frame name to use for forward kinematics.
         max_step_size: The maximum step size between joint configurations when interpolating paths.
     """
@@ -374,12 +374,17 @@ def visualizePath(
             goal_segments.append([frame_path[idx][:3, 3], frame_path[idx + 1][:3, 3]])
 
     path_segments = []
-    for idx in range(len(path.positions) - 1):
-        q_start = path.positions[idx]
-        q_end = path.positions[idx + 1]
-        frame_path = computeFramePath(scene, q_start, q_end, frame_name, max_step_size)
-        for idx in range(len(frame_path) - 1):
-            path_segments.append([frame_path[idx][:3, 3], frame_path[idx + 1][:3, 3]])
+    if path is not None:
+        for idx in range(len(path.positions) - 1):
+            q_start = path.positions[idx]
+            q_end = path.positions[idx + 1]
+            frame_path = computeFramePath(
+                scene, q_start, q_end, frame_name, max_step_size
+            )
+            for idx in range(len(frame_path) - 1):
+                path_segments.append(
+                    [frame_path[idx][:3, 3], frame_path[idx + 1][:3, 3]]
+                )
 
     if start_segments:
         viz.viewer.scene.add_line_segments(
@@ -396,9 +401,10 @@ def visualizePath(
             line_width=1.0,
         )
 
-    viz.viewer.scene.add_line_segments(
-        "/rrt/path",
-        points=np.array(path_segments),
-        colors=(100, 100, 0),
-        line_width=3.0,
-    )
+    if path_segments:
+        viz.viewer.scene.add_line_segments(
+            "/rrt/path",
+            points=np.array(path_segments),
+            colors=(100, 100, 0),
+            line_width=3.0,
+        )
