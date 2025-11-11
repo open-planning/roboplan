6f136b99694d49d7644bad6e2553fd00591472c9
Sebastian Castro <sebas.a.castro@gmail.com>
2025-11-11 08:00:30 -0500

    Test for pinocchio version

--- bindings/test/test_import.py
+++ bindings/test/test_import.py
@@ -4,6 +4,7 @@ Basic sanity checks for RoboPlan module.
 
 import importlib
 from importlib.metadata import version
+from packaging.version import parse
 
 
 def test_import_roboplan() -> None:
@@ -18,7 +19,8 @@ def test_roboplan_version() -> None:
 
 def test_import_pinocchio() -> None:
     assert importlib.util.find_spec("pinocchio")
-    importlib.import_module("pinocchio")
+    pinocchio = importlib.import_module("pinocchio")
+    assert parse(pinocchio.__version__) >= parse("3.8.0")
 
 
 def test_import_viser() -> None:
