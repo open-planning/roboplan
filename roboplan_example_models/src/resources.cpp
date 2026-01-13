1b84be59433374db4265880660fc709267f5e540
Erik Holum <eholum@gmail.com>
2026-01-13 10:00:03 -0500

    Move the lookup function to a different compilation unit

--- roboplan_example_models/src/resources.cpp
+++ roboplan_example_models/src/resources.cpp
@@ -5,21 +5,17 @@
 
 namespace roboplan::example_models {
 
-std::filesystem::path get_install_prefix() {
-  // If roboplan was installed using conda, get it from the prefix.
-  // Note that this will break in the event that you've built from source and are
-  // using conda yourself, but it's a better default than not supporting conda at all.
-  const auto conda_prefix = std::getenv("CONDA_PREFIX");
-  if (conda_prefix) {
-    return std::filesystem::path(conda_prefix);
-  }
+namespace anchor {
+  extern void example_models_location_anchor();
+}
 
+std::filesystem::path get_install_prefix() {
   // This would be a lot easier if it were an ament package, instead we use
   // dynamic linking to get the filesystem path of the example resources shared
   // object file.
   Dl_info dl_info;
-  dladdr((void*)get_install_prefix, &dl_info);
-  std::filesystem::path lib_path = dl_info.dli_fname;
+  dladdr((void*)&anchor::example_models_location_anchor, &dl_info);
+  std::filesystem::path lib_path = std::filesystem::canonical(dl_info.dli_fname);
 
   // Then we can just pull the relative path to the share directory
   // <install_directory>/lib/roboplan_example_models/<executable>
