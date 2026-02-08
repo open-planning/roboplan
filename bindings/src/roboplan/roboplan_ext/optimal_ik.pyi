c77428d7a57b0ea02f300016d219cf5ac9c288e5
Sebastian Castro <sebas.a.castro@gmail.com>
2026-02-07 21:53:12 -0500

    Remove random docstring for constructor

--- bindings/src/roboplan/roboplan_ext/optimal_ik.pyi
+++ bindings/src/roboplan/roboplan_ext/optimal_ik.pyi
@@ -29,8 +29,7 @@ class Task:
 class FrameTaskOptions:
     """Parameters for FrameTask."""
 
-    def __init__(self, position_cost: float = 1.0, orientation_cost: float = 1.0, task_gain: float = 1.0, lm_damping: float = 0.0) -> None:
-        """Constructor with custom parameters."""
+    def __init__(self, position_cost: float = 1.0, orientation_cost: float = 1.0, task_gain: float = 1.0, lm_damping: float = 0.0) -> None: ...
 
     @property
     def position_cost(self) -> float:
