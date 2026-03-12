84821ef94a2e0ac6499f02c502ab8ff8f8f81b29
Sebastian Castro <sebas.a.castro@gmail.com>
2026-03-11 21:43:04 -0400

    Move check to pixi

--- bindings/src/roboplan/roboplan_ext/__init__.pyi
+++ bindings/src/roboplan/roboplan_ext/__init__.pyi
@@ -1,8 +1,1 @@
-from . import (
-    core as core,
-    example_models as example_models,
-    optimal_ik as optimal_ik,
-    rrt as rrt,
-    simple_ik as simple_ik,
-    toppra as toppra
-)
+../../../build/cp311-cp311-linux_x86_64/roboplan_ext/__init__.pyi
